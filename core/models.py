79f9f88213ed6de94da5b86d5d7b4002caeeb195
Ed Summers <ehs@pobox.com>
2011-09-02 00:10:54 -0400

    added wishlist model, and a simplistic module books.py for looking up books by isbn at googlebooks and openlibrary

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Campaign(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=500, null=False)
     description = models.CharField(max_length=10000, null=False)
     target = models.FloatField()
@@ -10,8 +12,8 @@ class Campaign(models.Model):
     work = models.ForeignKey("Work", related_name="campaign")
  
 class Work(models.Model):
-    title = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=1000)
 
 class Edition(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -21,10 +23,17 @@ class Edition(models.Model):
     work = models.ForeignKey("Work", related_name="editions")
 
 class Identifier(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=10)
     value = models.CharField(max_length=500)
     edition = models.ForeignKey("Edition", related_name="identifiers")
 
 class Author(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=500)
     edition = models.ForeignKey("Edition", related_name="authors")
+
+class Wishlist(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, related_name='wishlist')
+    works = models.ManyToManyField('Work', related_name='wishlists')
