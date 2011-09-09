674686233fb4167657c96f62b8ee42917ba07a1b
Ed Summers <ehs@pobox.com>
2011-09-09 01:38:28 -0400

    lots of changes to support dynamically loading from openlibrary based on an isbn...which still needs some work to prevent duplication

--- core/books.py
+++ core/books.py
@@ -1,101 +1,99 @@
-from django.conf import settings
+"""
+The module handles fetching books from OpenLibrary and adding them 
+to the local database.
+"""
 
 import json
 import logging
 
 import requests
+from django.conf import settings
 
-def get_edition(isbn):
+from regluit.core import models
 
-    # lookup the isbn at googlebooks
-    results = googlebooks_search("isbn:%s" % isbn)
-    if results['totalItems'] == 0:
-        return None
-    elif results['totalItems'] > 1:
-        return LookupFailure("multiple results for isbn %s" % isbn)
 
-    # get the edition detail from google
-    google_id = results['items'][0]['id']
-    i = googlebooks_volume(google_id)
+def add_book(isbn):
+    url = "http://openlibrary.org/api/books"
+    bibkeys = "ISBN:%s" % isbn
+    params = {"bibkeys": bibkeys, "jscmd": "details", "format": "json"}
+    results = get_json(url, params)
+    if results.has_key(bibkeys):
+        return save_edition(results[bibkeys]['details'])
+    return None
 
-    # normalize some of the googlebooks data
-    edition = {
-        'title': i['volumeInfo']['title'],
-        'authors': i['volumeInfo']['authors'],
-        'description': i['volumeInfo']['description'],
-        'categories': i['volumeInfo']['categories'],
-        'language': i['volumeInfo']['language'],
-        'publisher': i['volumeInfo']['publisher'],
-        'publication_date': i['volumeInfo']['publishedDate'],
-        'page_count': i['volumeInfo']['pageCount'],
-        'google_books_id': i['id'],
-        'cover_image_url': i['volumeInfo']['imageLinks']['thumbnail'],
-        'isbn_10': [],
-        'isbn_13': []
-    }
 
-    for identifier in i['volumeInfo'].get('industryIdentifiers', []):
-        if identifier['type'] == 'ISBN_10':
-            edition['isbn_10'].append(identifier['identifier'])
-        elif identifier['type'] == 'ISBN_13':
-            edition['isbn_13'].append(identifier['identifier'])
+def save_edition(edition_data):
+    edition = models.Edition()
+    edition.title = edition_data.get('title')
+    edition.description = edition_data.get('description')
+    edition.publisher = first(edition_data, 'publishers')
+    edition.publication_date = edition_data.get('publish_date')
+    edition.save()
+
+    for work_data in edition_data.get('works', []):
+        save_work(work_data['key'], edition)
+
+    for isbn_10 in edition_data.get('isbn_10', []):
+        models.EditionIdentifier.objects.get_or_create(name='isbn_10', value=isbn_10, edition=edition)
+
+    for isbn_13 in edition_data.get('isbn_13', []):
+        models.EditionIdentifier.objects.get_or_create(name='isbn_13', value=isbn_13, edition=edition)
+
+    for cover_id in edition_data.get('covers', []):
+        models.EditionCover.objects.get_or_create(openlibrary_id=cover_id, edition=edition)
 
     return edition
 
 
-def get_work(isbn):
-    return {'title': 'Neuromancer'}
+def save_work(work_key, edition):
+    url = "http://openlibrary.org" + work_key
+    work_data = get_json(url)
+
+    work = models.Work()
+    work.title = work_data.get('title')
+    work.openlibrary_id = work_key
+    work.save()
+    
+    for author_data in work_data.get('authors', []):
+        save_author(author_data['author']['key'], work)
+
+    for subject_name in work_data.get('subjects', []):
+        subject, created = models.Subject.objects.get_or_create(name=subject_name)
+        work.subjects.add(subject)
+
+    work.editions.add(edition)
+
+    return work
 
 
-def googlebooks_search(q):
-    params = {"q": q, "key": settings.GOOGLE_BOOKS_API_KEY}
-    url = 'https://www.googleapis.com/books/v1/volumes'
-    return _get_json(url, params)
+def save_author(author_key, work):
+    url = "http://openlibrary.org" + author_key
+    author_data = get_json(url)
+
+    author = models.Author()
+    author.name = author_data['name']
+    author.save()
+
+    author.works.add(work)
+
+    return author
 
 
-def googlebooks_volume(google_books_id):
-    params = {"key": settings.GOOGLE_BOOKS_API_KEY}
-    url = "https://www.googleapis.com/books/v1/volumes/%s" % google_books_id
-    return _get_json(url, params)
+def first(dictionary, key):
+    l = dictionary.get(key, [])
+    if len(l) == 0: return None
+    return l[0]
 
 
-def _get_json(url, params):
-    response = requests.get(url, params=params)
+def get_json(url, params={}):
+    headers = {'User-Agent': 'unglue.it bot', 'Accept': 'application/json'}
+    response = requests.get(url, params=params, headers=headers)
     if response.status_code == 200:
         return json.loads(response.content)
     else:
         logging.error("unexpected HTTP response: %s" % response)
         raise LookupFailure("GET failed: url=%s and params=%s" % (url, params))
 
+
 class LookupFailure(Exception):
     pass
-
-
-if __name__ == "__main__":
-    # list of books from Amanda
-    isbns = [
-        # amanda's first list
-        "0811216993", "0811216713", "1564780880", "0156709902",
-        "0803260970", "0231145365", "0745647863", "0393302318",
-        "1590171101", "015602909X", "0156576813", "0156982900",
-        "0156189798", "0553380834", "0375706682", "055380491X",
-        "0553378589", "0684831074", "0761148574", "1563053381",
-        "0520256093", "0679442707", "0375754741",
-
-        # amanda's second list
-        "0547773986", "0719595800", "1582432104", "0156372088",
-        "0139391401", "0300022867", "0300084587", "0691063184",
-        "0691004765", "0940242753", "0671240773", "0060852240",
-        "0307270807", "0520047834", "0471609978", "0030839939",
-        "0465027822", "0465028381", "0716760355", "1597261076",
-        "1559638583", "0393014886", "0452011752", "0151400563",
-        "0812536363", "0441007465", "0441569595", "0441012035",
-        "0393311198", "0525200150", "0375704078", "0773723749",
-        "0760759146", "0441478123", "0553383043", "006076029X",
-        "0802714544", "0743442539", "0743431677",
-    ]
-
-    for isbn in isbns:
-        print get_edition(isbn)
-
-
