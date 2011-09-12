4ddaf1f3cb776ac3cd4f57d0229412c97c3f3089
Ed Summers <ehs@pobox.com>
2011-09-12 15:23:33 +0000

    renamed user view to contributor, since that is what eric has been calling it

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns(
     "regluit.frontend.views",
     url(r"^$", "home", name="home"),
-    url(r"^user/(?P<username>.+)/$", "user", name="user"),
+    url(r"^contributor/(?P<contributor_username>.+)/$", "contributor", name="contributor"),
 )
