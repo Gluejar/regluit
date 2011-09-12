392f746b2fcd8eb38a074d34dc31cbee30ba6e8e
Ed Summers <ehs@pobox.com>
2011-09-12 18:20:36 +0000

    should have been supporter not contributor ...

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns(
     "regluit.frontend.views",
     url(r"^$", "home", name="home"),
-    url(r"^contributor/(?P<contributor_username>.+)/$", "contributor", name="contributor"),
+    url(r"^supporter/(?P<supporter_username>.+)/$", "supporter", name="supporter"),
 )
