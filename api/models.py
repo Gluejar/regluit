0846bab210f5b4bc6d8ddf513fe91a197afa7a6e
Raymond Yee <raymond.yee@gmail.com>
2011-09-12 14:50:29 -0700

    implementation of read-only api for Work, Edition, Subject, Campaign, Author added a check_db command to check database integrity of regluit.core.models

--- api/models.py
+++ api/models.py
@@ -1,7 +1,32 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 from regluit.core import models
 
 class WorkResource(ModelResource):
     class Meta:
         queryset = models.Work.objects.all()
         resource_name = 'work'
+        
+class EditionResource(ModelResource):
+    work = fields.ForeignKey(WorkResource, 'work')
+    class Meta:
+        queryset = models.Edition.objects.all()
+        resource_name = 'edition'
+
+class CampaignResource(ModelResource):
+    work = fields.ForeignKey(WorkResource, 'work')
+    class Meta:
+        queryset = models.Campaign.objects.all()
+        resource_name = 'campaign'
+        
+class AuthorResource(ModelResource):
+    works = fields.ToManyField(WorkResource, 'works')
+    class Meta:
+        queryset = models.Author.objects.all()
+        resource_name = 'author'
+
+class SubjectResource(ModelResource):
+    works = fields.ToManyField(WorkResource, 'works')
+    class Meta:
+        queryset = models.Subject.objects.all()
+        resource_name = 'subject'
