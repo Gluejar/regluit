49bd1d38fc40be19d0e08b62186aeedad2fbc8e9
Ed Summers <ehs@pobox.com>
2011-09-15 22:33:05 +0000

    added resource filtering to be able to lookup campaigns based on isbn with /api/v1/campaign/?format=json&work__editions__isbn_10=1590171101

--- api/urls.py
+++ api/urls.py
@@ -1,22 +1,18 @@
 from django.conf.urls.defaults import *
 from tastypie.api import Api
 
-from regluit.api.models import UserResource, WorkResource, EditionResource, CampaignResource, AuthorResource, SubjectResource, EditionCoverResource, WishlistResource
-from regluit.api.models import IsbnResource
+from regluit.api import resources
 
-v1_api = Api(api_name='v1')  # how set api_name to None so that we don't get URIs like /api/v1 but more like /api/ Don't think there is a way...
-v1_api.register(UserResource())
-v1_api.register(WorkResource())
-v1_api.register(EditionResource())
-v1_api.register(EditionCoverResource())
-v1_api.register(CampaignResource())
-v1_api.register(AuthorResource())
-v1_api.register(SubjectResource())
-v1_api.register(WishlistResource())
-v1_api.register(IsbnResource())
+v1_api = Api(api_name='v1')
+v1_api.register(resources.UserResource())
+v1_api.register(resources.WorkResource())
+v1_api.register(resources.EditionResource())
+v1_api.register(resources.EditionCoverResource())
+v1_api.register(resources.CampaignResource())
+v1_api.register(resources.AuthorResource())
+v1_api.register(resources.SubjectResource())
+v1_api.register(resources.WishlistResource())
 
 urlpatterns = patterns('',
-    url(r'^editions/$', 'regluit.api.views.editions', name="editions"),
-    url(r'^isbn/(?P<isbn>\w+)/$','regluit.api.views.isbn', name="isbn"),
     (r'^', include(v1_api.urls)),
 )
