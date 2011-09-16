6d6af629f4a4d89d987246e120688253c03d61ca
Ed Summers <ehs@pobox.com>
2011-09-15 22:05:47 -0400

    added some api unit tests

--- api/tests.py
+++ api/tests.py
@@ -1,23 +1,43 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+import json
+import datetime
 
 from django.test import TestCase
+from django.test.client import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from regluit.core import bookloader, models
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class CampaignTests(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
+    def setUp(self):
+        edition = bookloader.add_book(isbn='0441012035')
+        campaign = models.Campaign.objects.create(
+            name=edition.work.title,
+            work=edition.work, 
+            description='Test Campaign',
+            deadline=datetime.datetime.now(),
+            target=1000.0,
+        )
+        self.client = Client()
 
+    def test_campaigns(self):
+        r = self.client.get('/api/v1/campaign/', data={'format': 'json'})
+        self.assertEqual(r.status_code, 200)
+        j = json.loads(r.content)
+        self.assertEqual(len(j['objects']), 1)
+        self.assertEqual(j['objects'][0]['name'], 'Neuromancer')
+        self.assertEqual(j['objects'][0]['work'], '/api/v1/work/1/')
+
+    def test_campaign(self):
+        r = self.client.get('/api/v1/campaign/1/', data={'format': 'json'})
+        self.assertEqual(r.status_code, 200)
+        j = json.loads(r.content)
+        self.assertEqual(j['name'], 'Neuromancer')
+        self.assertEqual(j['work'], '/api/v1/work/1/')
+
+    def test_campaign_lookup_by_isbn(self):
+        url = '/api/v1/campaign/?work__editions__isbn_10=0441012035'
+        r = self.client.get('/api/v1/campaign/', data={'format': 'json', 'work__editions__isbn_10': '0441012035'})
+        self.assertEqual(r.status_code, 200)
+        j = json.loads(r.content)
+        self.assertEqual(len(j['objects']), 1)
+        self.assertEqual(j['objects'][0]['name'], 'Neuromancer')
