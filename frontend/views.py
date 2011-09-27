195f6db36059bb57035bbe0c308b8e499fdab847
Ed Summers <ehs@pobox.com>
2011-09-27 09:27:15 +0000

    redirect to supporter page if logged in

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,16 @@
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
+
 from regluit.core import models
 
 def home(request):
+    # if the user is logged in send them to their supporter page
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('supporter',
+            args=[request.user.username]))
     return render_to_response('home.html', 
         {},
         context_instance=RequestContext(request)
