88abca0b33a1bd2cc5c53c86a7ff5cb509d3edfb
eric <eric@hellman.net>
2011-09-29 19:28:16 -0400

    quote style

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
-    "regluit.frontend.views",
-    url(r"^$", "home", name="home"),
-    url(r"^supporter/(?P<supporter_username>.+)/$", "supporter", name="supporter"),
-    url(r"^privacy$", "textpage", {'page': 'privacy'}, name="privacy"),
+    'regluit.frontend.views',
+    url(r'^$', 'home', name='home'),
+    url(r'^supporter/(?P<supporter_username>.+)/$', 'supporter', name='supporter'),
+    url(r'^privacy$', 'textpage', {'page': 'privacy'}, name='privacy'),
 )
