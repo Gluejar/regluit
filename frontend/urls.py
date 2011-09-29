409df080ced2066c7131759c5cba34f73eba6ba4
Ed Summers <ehs@pobox.com>
2011-09-29 02:23:50 -0400

    got search passably working and a somewhat functional ajax add to list

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     "regluit.frontend.views",
     url(r"^$", "home", name="home"),
     url(r"^supporter/(?P<supporter_username>.+)/$", "supporter", name="supporter"),
     url(r"^search/$", "search", name="search"),
-    url(r"^privacy/$", direct_to_template, {"template": "privacy.html"},
+    url(r"^privacy/$", TemplateView.as_view(template_name="privacy.html"),
         name="privacy"),
+    url(r"^wishlist/$", "wishlist", name="wishlist"),
 )
--- frontend/views.py
+++ frontend/views.py
@@ -2,9 +2,13 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response, get_object_or_404
 
-from regluit.core import models, search
+from regluit.core import models, bookloader
+from regluit.core.search import gluejar_search
 
 def home(request):
     if request.user.is_authenticated():
@@ -14,18 +18,46 @@ def home(request):
 
 def supporter(request, supporter_username):
     supporter = get_object_or_404(User, username=supporter_username)
-    campaigns = models.Campaign.objects.all()
+    wishlist = supporter.wishlist
     context = {
         "supporter": supporter,
-        "campaigns": campaigns,
+        "wishlist": wishlist,
     }
     return render(request, 'supporter.html', context)
 
 def search(request):
     q = request.GET.get('q', None)
-    results = search.gluejar_search(q)
+    results = gluejar_search(q)
+
+    # flag search result as on wishlist
+    # TODO: make this better and faster
+    if request.user:
+        for result in results:
+            if not result.has_key('isbn_10'):
+                continue
+            work = models.Work.get_by_isbn(result['isbn_10'])
+            if work and work in request.user.wishlist.works.all():
+                result['on_wishlist'] = True
+            else:
+                result['on_wishlist'] = False
+
     context = {
         "q": q,
         "results": results,
     }
     return render(request, 'search.html', context)
+
+@csrf_exempt
+@require_POST
+@login_required
+def wishlist(request):
+    isbn = request.POST.get('isbn', None)
+    edition = models.Edition.get_by_isbn(isbn)
+    if not edition:
+        print "loading book"
+        edition = bookloader.add_book(isbn)
+    if edition:
+        print "adding edition"
+        request.user.wishlist.works.add(edition.work)
+    # TODO: redirect to work page, when it exists
+    return HttpResponseRedirect('/')
