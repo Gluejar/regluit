23c4c48946b86972f8c457f5195b8700241b64de
Ed Summers <ehs@pobox.com>
2011-09-28 21:54:50 -0400

    simplified privacy view to use direct_to_template generic view; now use render() from django 1.3 which is easier

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns(
     "regluit.frontend.views",
     url(r"^$", "home", name="home"),
     url(r"^supporter/(?P<supporter_username>.+)/$", "supporter", name="supporter"),
     url(r"^search/$", "search", name="search"),
-    url(r"^privacy$", "textpage", {'page': 'privacy'}, name="privacy"),
+    url(r"^privacy/$", direct_to_template, {"template": "privacy.html"},
+        name="privacy"),
 )
--- frontend/views.py
+++ frontend/views.py
@@ -4,31 +4,28 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response, get_object_or_404
 
-from regluit.core import models
+from regluit.core import models, search
 
 def home(request):
-    # if the user is logged in send them to their supporter page
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('supporter',
             args=[request.user.username]))
-    return render_to_response('home.html', 
-        {},
-        context_instance=RequestContext(request)
-    )
+    return render(request, 'home.html')
 
 def supporter(request, supporter_username):
     supporter = get_object_or_404(User, username=supporter_username)
     campaigns = models.Campaign.objects.all()
-    return render_to_response('supporter.html',
-        {"supporter": supporter, "campaigns": campaigns},
-        context_instance=RequestContext(request)
-    )
-
-def textpage(request, page):	
-    return render_to_response(page + '.html', 
-        {},
-        context_instance=RequestContext(request)
-    )
+    context = {
+        "supporter": supporter,
+        "campaigns": campaigns,
+    }
+    return render(request, 'supporter.html', context)
 
 def search(request):
-    return render(request, 'search.html', {})
+    q = request.GET.get('q', None)
+    results = search.gluejar_search(q)
+    context = {
+        "q": q,
+        "results": results,
+    }
+    return render(request, 'search.html', context)
