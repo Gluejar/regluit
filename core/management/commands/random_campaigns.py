3773a6dd9ccf8d3d0d0ddb29dcb2b4bedc94c678
Andromeda Yelton <andromeda.yelton2gmail.com>
2011-10-13 12:21:53 -0400

    switched to decimal to address python 2.6/2.7 compatibility bug

--- core/management/commands/random_campaigns.py
+++ core/management/commands/random_campaigns.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from random import randint, randrange
 from datetime import datetime, timedelta
 
@@ -18,7 +19,7 @@ class Command(BaseCommand):
             campaign.description = "Test Campaign"
 
             # random campaign target between $200 and $10,000
-            campaign.target = float(randint(200,10000))
+            campaign.target = Decimal(str(randint(200,10000)))
 
             # random deadline between 5 days from now and 180 days from now
             now = datetime.now()
