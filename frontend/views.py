7cf40e88de0a568012ad0703e698368f696dfb3b
Ed Summers <ehs@pobox.com>
2011-11-07 16:04:37 -0500

    need to import Q

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,7 @@
 import logging
 from decimal import Decimal as D
 
+from django.db.models import Q
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
