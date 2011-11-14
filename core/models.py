5211e774a8472cd82d0b3af9a33ac2badd135b45
eric <eric@hellman.net>
2011-11-14 10:17:31 -0500

    fixed librarything username length. migrate your schemas!

--- core/models.py
+++ core/models.py
@@ -277,7 +277,7 @@ class UserProfile(models.Model):
     home_url =  models.URLField(blank=True)
     twitter_id =  models.CharField(max_length=15, blank=True)
     facebook_id =  models.PositiveIntegerField(null=True)
-    librarything_id =  models.CharField(max_length=15, blank=True)
+    librarything_id =  models.CharField(max_length=31, blank=True)
     
     goodreads_user_id = models.CharField(max_length=32, null=True, blank=True)
     goodreads_user_name = models.CharField(max_length=200, null=True, blank=True)
