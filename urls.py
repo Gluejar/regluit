e7bcab81125bd3d6cf9391912f9a9f26f28bb087
eric <eric@hellman.net>
2011-11-18 18:08:42 -0500

    one more thing to turn off to get app to run with admin switched off

--- urls.py
+++ urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     (r'', include('regluit.payment.urls')),
     (r'^selectable/', include('selectable.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^admin/', include(admin.site.urls)),
 )
