d2f1f6c1ebdcb383ccd4906eed3c9ae91f277706
Ed Summers <ehs@pobox.com>
2011-11-19 11:58:59 -0500

    would help if I added the admin module :-)

---
+++ admin.py
@@ -0,0 +1,62 @@
+from django.contrib.admin import ModelAdmin
+from django.contrib.admin.sites import AdminSite
+
+from regluit.core import models
+
+class RegluitAdmin(AdminSite):
+    password_change_template = 'registration/password_change_form'
+
+class ClaimAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+class RightsHolderAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+class PremiumAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+class CampaignAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+class WorkAdmin(ModelAdmin):
+    search_fields = ('title',)
+    ordering = ('title',)
+    list_display = ('title', 'created')
+    date_hierarchy = 'created'
+    fields = ('title',)
+
+class AuthorAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+class SubjectAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+    ordering = ('name',)
+
+class EditionAdmin(ModelAdmin):
+    list_display = ('title', 'publisher', 'created')
+    date_hierarchy = 'created'
+    ordering = ('title',)
+
+class EbookAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+    ordering = ('edition__title',)
+
+class WishlistAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+class UserProfileAdmin(ModelAdmin):
+    date_hierarchy = 'created'
+
+admin_site = RegluitAdmin("Admin")
+
+admin_site.register(models.Work, WorkAdmin)
+admin_site.register(models.Claim, ClaimAdmin)
+admin_site.register(models.RightsHolder, RightsHolderAdmin)
+admin_site.register(models.Premium, PremiumAdmin)
+admin_site.register(models.Campaign, CampaignAdmin)
+admin_site.register(models.Author, AuthorAdmin)
+admin_site.register(models.Subject, SubjectAdmin)
+admin_site.register(models.Edition, EditionAdmin)
+admin_site.register(models.Ebook, EbookAdmin)
+admin_site.register(models.Wishlist, WishlistAdmin)
+admin_site.register(models.UserProfile, UserProfileAdmin)
