9812008a36f946d9e28a3e6e35e8f3c1699c1fdb
Raymond Yee <raymond.yee@gmail.com>
2011-11-22 16:21:38 -0800

    Need to set BASE_URL to allow PayPal IPN to be handled on please

--- settings/prod.py
+++ settings/prod.py
@@ -83,7 +83,10 @@ FREEBASE_PASSWORD = ''
 
 # send celery log to Python logging
 CELERYD_HIJACK_ROOT_LOGGER = False
-BASE_URL = 'http://0.0.0.0/'
+
+# BASE_URL is a hard-coding of the domain name for site and used for PayPal IPN (no ending /)
+# Next step to try https
+BASE_URL = 'http:/please.unglueit.com'
 
 # use redis for production queue
 BROKER_TRANSPORT = "redis"
