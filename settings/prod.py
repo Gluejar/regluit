8e3bd565579086113e318d58db5025714c7accd6
Raymond Yee <raymond.yee@gmail.com>
2011-11-22 16:03:05 -0800

    Setting PAYPAL_GLUEJAR_EMAIL -- the missing value might have the reason why Paypal not working on please

--- settings/prod.py
+++ settings/prod.py
@@ -67,7 +67,7 @@ PAYPAL_SANDBOX_PASSWORD = ''
 PAYPAL_BUYER_LOGIN =''
 PAYPAL_BUYER_PASSWORD = ''
 
-PAYPAL_GLUEJAR_EMAIL = ""
+PAYPAL_GLUEJAR_EMAIL = "glueja_1317336101_biz@gluejar.com"
 
 # for test purposes have a single RH paypal email
 PAYPAL_TEST_RH_EMAIL = "rh1_1317336251_biz@gluejar.com"
