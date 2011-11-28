b3d7a7835f81f10d91ddab20176bf221a1f43be0
Raymond Yee <raymond.yee@gmail.com>
2011-11-28 10:27:20 -0800

    http:/please.unglueit.com -> http://please.unglueit.com in prod.py

--- settings/prod.py
+++ settings/prod.py
@@ -86,7 +86,7 @@ CELERYD_HIJACK_ROOT_LOGGER = False
 
 # BASE_URL is a hard-coding of the domain name for site and used for PayPal IPN
 # Next step to try https
-BASE_URL = 'http:/please.unglueit.com'
+BASE_URL = 'http://please.unglueit.com'
 
 # use redis for production queue
 BROKER_TRANSPORT = "redis"
