a4d609c76fdbe4e9ffa53256dfffa7a83d766829
Andromeda Yelton <andromeda.yelton@gmail.com>
2011-12-13 13:32:29 -0500

    added google books branding to extra search box on empty search results page

--- static/js/watermark_change.js
+++ static/js/watermark_change.js
@@ -7,3 +7,14 @@ function imgblur(pos){
 function imgfocus(){
 	document.getElementById('watermark').style.background="";
 }
+
+// special case for search box which is repeated on empty search results page
+function imgblurempty(pos){
+	if (document.getElementById('watermarkempty').value == "") {
+		document.getElementById('watermarkempty').style.background="white url('/static/images/google_watermark.gif') no-repeat "+pos+"px center";
+	}
+}
+
+function imgfocusempty(){
+	document.getElementById('watermarkempty').style.background="";
+}
--- static/js/watermark_init.js
+++ static/js/watermark_init.js
@@ -3,4 +3,8 @@ $j(document).ready(function() {
 	if (!$j('#watermark').val()) {
 		$j('#watermark').css({"background": "white url('/static/images/google_watermark.gif') no-repeat left center"});
 	}
+	// special case for search box which is repeated on empty search results page
+	if (!$j('#watermarkempty').val()) {
+		$j('#watermarkempty').css({"background": "white url('/static/images/google_watermark.gif') no-repeat left center"});
+	}
 });
