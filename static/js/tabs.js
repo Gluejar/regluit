754fcc394e1792060f59deed62f478b6e7e0d2fd
Andromeda Yelton <andromeda.yelton@gmail.com>
2011-12-14 16:49:36 -0500

    eleventy billion css tweaks, part one

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -9,27 +9,30 @@
         );
         $j('.tabs1').click(function(){
             $j('.tabs').find('.active').removeClass('active');
-            $j(this).addClass('active');
+            $j('li.tabs1').addClass('active');
             $j('.content-block-content').find('.active').removeClass('active');
             $j('.tabs-1').addClass('active').show(300);
             $j('.tabs-2').hide(200);
             $j('.tabs-3').hide(200);
+            $j('#tabs').removeClass('wantto').removeClass('ungluing').addClass('unglued');
         });
         $j('.tabs2').click(function(){
             $j('.tabs').find('.active').removeClass('active');
-            $j(this).addClass('active');
+            $j('li.tabs2').addClass('active');
             $j('.content-block-content').find('.active').removeClass('active');
             $j('.tabs-2').addClass('active').show(300);
             $j('.tabs-1').hide(200);
             $j('.tabs-3').hide(200);
+            $j('#tabs').removeClass('unglued').removeClass('wantto').addClass('ungluing');
         });
         $j('.tabs3').click(function(){
             $j('.tabs').find('.active').removeClass('active');
-            $j(this).addClass('active');
+            $j('li.tabs3').addClass('active');
             $j('.content-block-content').find('.active').removeClass('active');
             $j('.tabs-3').addClass('active').show(300);
             $j('.tabs-2').hide(200);
             $j('.tabs-1').hide(200);
+            $j('#tabs').removeClass('unglued').removeClass('ungluing').addClass('wantto');
             });
         $j('.empty-wishlist span.bounce-search').click(function(){
             $j('div.js-search-inner').effect("bounce", 500, function() {
--- static/js/watermark_init.js
+++ static/js/watermark_init.js
@@ -2,6 +2,7 @@ var $j = jQuery.noConflict();
 $j(document).ready(function() {
 	if (!$j('#watermark').val()) {
 		$j('#watermark').css({"background": "white url('/static/images/google_watermark.gif') no-repeat left center"});
+		$j('.inputalign > #watermark').css({"background": "white url('/static/images/google_watermark.gif') no-repeat 15px center"});
 	}
 	// special case for search box which is repeated on empty search results page
 	if (!$j('#watermarkempty').val()) {
