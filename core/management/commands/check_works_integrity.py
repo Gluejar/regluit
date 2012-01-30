078c853bafc5b6e3be058d2b899be8f07af517ef
Raymond Yee <raymond.yee@gmail.com>
2012-01-30 12:30:18 -0800

    Now listing number of editions w/o works and number of identifiers not tied to work

--- core/management/commands/check_works_integrity.py
+++ core/management/commands/check_works_integrity.py
@@ -5,4 +5,7 @@ class Command(BaseCommand):
     help = "Do a few integrity checks on Works, Editions, and Identifiers"
     
     def handle(self, **options):
-        print "Number of Works without identifiers:", models.Work.objects.filter(identifiers__isnull=True).count()
+        print "Number of Works without identifiers: ", models.Work.objects.filter(identifiers__isnull=True).count()
+        print "Number of Identifiers not tied to Works (should be 0): ",  \
+            models.Identifier.objects.filter(work__isnull=True).count()
+        print "Number of Editions not tied to a Work (should be 0): ", models.Edition.objects.filter(work__isnull=True).count()
