e4285400eabc8cb1665f9c17b46bc82b6512d44a
Raymond Yee <raymond.yee@gmail.com>
2012-02-08 09:19:17 -0800

    Changed the isbn.ISBN class to not throw exceptions on invalid ISBNs but rather return None at various places

--- core/tests.py
+++ core/tests.py
@@ -344,10 +344,11 @@ class ISBNTest(TestCase):
         
         isbn_python_10 = isbn.ISBN(python_10)
         isbn_python_13 = isbn.ISBN(python_13)
-        # raise exception for wrong length or invalid characters
-        self.assertRaises(isbn.ISBNException, isbn.ISBN, "978-0-M72-32978-X")
+        # return None for invalid characters
+        self.assertEqual(None, isbn.ISBN("978-0-M72-32978-X").to_string('13'))
+        self.assertEqual(isbn.ISBN("978-0-M72-32978-X").valid, False)
         # check that only ISBN 13 starting with 978 or 979 are accepted
-        self.assertRaises(isbn.ISBNException, isbn.ISBN, "111-0-M72-32978-X")
+        self.assertEqual(None, isbn.ISBN("111-0-M72-32978-X").to_string())
         
         # right type?
         self.assertEqual(isbn_python_10.type, '10')
@@ -370,7 +371,7 @@ class ISBNTest(TestCase):
         # complain if one tries to get ISBN-10 for a 979 ISBN 13
         # making up a 979 ISBN
         isbn_979 = isbn.ISBN("979-1-234-56789-0").validate()
-        self.assertRaises(isbn.ISBNException, isbn_979.to_string, '10')
+        self.assertEqual(isbn_979.to_string('10'), None)
         
         # check casting to string -- ISBN 13
         self.assertEqual(str(isbn.ISBN(python_10)), '0672329786')
