60cd80b93b87b929a27ae8b48d3391ea30430d06
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-02-14 11:57:07 -0500

    moving from bind to on, speeding up, targeting to panelview only

--- static/js/greenpanel.js
+++ static/js/greenpanel.js
@@ -1,15 +1,14 @@
-var $j =jQuery.noConflict();
-$j(document).ready(function(){
-	$j('div.book-list').bind("mouseenter", (function()
-	{
+var $j = jQuery.noConflict();
+
+$j().ready(function(){
+	var contentblock = $j('#content-block');
+	contentblock.on('mouseenter', 'div.book-list.panelview', function() {
 		$j(this).children('.panelfront').removeClass('side1').addClass('side2');
 		$j(this).children('.panelback').removeClass('side2').addClass('side1');
-	}));
+	});
 				  
-	$j('div.book-list').bind("mouseleave", (function()
-	{
+	contentblock.on('mouseleave', 'div.book-list.panelview', function() {
 		$j(this).children('.panelback').removeClass('side1').addClass('side2');
 		$j(this).children('.panelfront').removeClass('side2').addClass('side1');
-	}));
-				  
+	});			  
 });
