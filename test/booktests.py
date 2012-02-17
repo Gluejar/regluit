2e079b2c2efd8c7b47a0063c12a4fd897aa96f6c
Raymond Yee <raymond.yee@gmail.com>
2012-02-17 10:30:09 -0800

    Now I have booktests to recalculate clusters

--- test/booktests.py
+++ test/booktests.py
@@ -85,25 +85,99 @@ def load_gutenberg_books(fname="/Users/raymondyee/D/Document/Gluejar/Gluejar.git
         else:
             logger.info("%d null seed_isbn: ebook %s", i, ebook)
 
-def cluster_status():
+def cluster_status(max_num=None):
     """Look at the current Work, Edition instances to figure out what needs to be fixed"""
     results = OrderedDict([
         ('number of Works', models.Work.objects.count()),
-        ('number of Editions', models.Edition.objects.count())
+        ('number of Works w/o Identifier', models.Work.objects.filter(identifiers__isnull=True).count()),
+        ('number of Editions', models.Edition.objects.count()),
+        ('number of Editions with ISBN', models.Edition.objects.filter(identifiers__type='isbn').count()),
+        ('number of Editions without ISBNs', models.Edition.objects.exclude(identifiers__type='isbn').count()),
         ('number of Edition that have both Google Books id and ISBNs',
              models.Edition.objects.filter(identifiers__type='isbn').filter(identifiers__type='goog').count()),
         ('number of Editions with Google Books IDs but not ISBNs',
              models.Edition.objects.filter(identifiers__type='goog').exclude(identifiers__type='isbn').count()),
         ])
     
-    # What needs to be done to recluster editions?
+    # models.Identifier.objects.filter(type='isbn').values_list('value', 'edition__id', 'edition__work__id', 'edition__work__language').count()
+    # 4 classes -- Edition have ISBN or not & ISBN is recognized or not by LT
+    # a) ISBN recognized by LT, b) ISBN not recognized by LT, c) no ISBN at all
+    
+    # [w._meta.get_all_related_objects() for w in works_no_ids] -- try to figure out whether any related objects before deleting
     
     # Are there Edition without ISBNs?  Look up the corresponding ISBNs from Google Books and Are they all singletons?
     
     # identify Editions that should be merged (e.g., if one Edition has a Google Books ID and another Edition has one with
     # an ISBN tied to that Google Books ID)
+
+
+    from collections import defaultdict
+    import shutil
+    import time
+    from collections import namedtuple
     
-    return results
+    # let's form a key to map all the Editions into
+    # (lt_work_id (or None), lang, ISBN (if lt_work_id is None or None if we don't know it), ed_id (or None) )
+    
+    work_clusters = defaultdict(set)
+    current_map = defaultdict(set)
+    
+    backup = '/Users/raymondyee/D/Document/Gluejar/Gluejar.github/regluit/experimental/lt_data_back.json'
+    fname = '/Users/raymondyee/D/Document/Gluejar/Gluejar.github/regluit/experimental/lt_data.json'
+    
+    EdInfo = namedtuple('EdInfo', ['isbn', 'ed_id', 'ed_title', 'work_id', 'lang'])
+    
+    shutil.copy(fname, backup)
+        
+    lt = bookdata.LibraryThing(fname)
+
+    try:
+        input_file = open(fname, "r")
+        success = lt.load()
+        print "success: %s" % (success)
+        input_file.close()
+    except Exception, e:
+        print e
+    
+    for (i, (isbn, ed_id, ed_title, work_id, lang)) in enumerate(
+        islice(models.Identifier.objects.filter(type='isbn').values_list('value', 'edition__id',
+                'edition__title', 'edition__work__id', 'edition__work__language'), max_num)):
+        
+        lt_work_id = lt.thingisbn(isbn, return_work_id=True)
+        key = (lt_work_id, lang, isbn if lt_work_id is None else None, None)
+        print i, isbn, lt_work_id, key
+        work_clusters[key].add(EdInfo(isbn=isbn, ed_id=ed_id, ed_title=ed_title, work_id=work_id, lang=lang))
+        current_map[work_id].add(key)
+    
+    lt.save()
+    
+    # Now add the Editions without any ISBNs
+    print "editions w/o isbn"
+    for (i, (ed_id, ed_title, work_id, lang)) in enumerate(
+        islice(models.Edition.objects.exclude(identifiers__type='isbn').values_list('id',
+                'title', 'work__id', 'work__language'), None)):
+        
+        key = (None, lang, None, ed_id)
+        print i, ed_id, ed_title, key
+        work_clusters[key].add(EdInfo(isbn=None, ed_id=ed_id, ed_title=ed_title, work_id=work_id, lang=lang))
+        current_map[work_id].add(key)
+
+    print "number of clusters", len(work_clusters)
+    
+    s = {'work_clusters':work_clusters, 'current_map':current_map, 'results':results}
+    
+    #
+    print "new clusters that map over more than one existing Work", \
+    [(k, len(set(([e.work_id for e in v])))) for (k,v) in s['work_clusters'].items() if len(set(([e.work_id for e in v]))) <> 1 ]
+    
+    m = current_map
+    print "existing Works that contain editions from more than 1 new cluster", \
+        sorted([k for (k,v) in m.items() if len(v) > 1])
+    
+    return s
+
+def all_editions():
+    pass
     
 
         
