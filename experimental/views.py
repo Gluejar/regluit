5a9c8fe207d53cc20c60a562dc6dc3b3050cf23d
Raymond Yee <raymond.yee@gmail.com>
2012-03-02 09:31:13 -0800

    Simple experimental view

--- experimental/views.py
+++ experimental/views.py
@@ -1,15 +1,18 @@
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.test.utils import setup_test_environment
+
+from regluit.core import models
+
 from unittest import TestResult
 
 import logging
 logger = logging.getLogger(__name__)
 
 def testExperimental(request):
-    return HttpResponse("hi there")
+    number_of_works = models.Work.objects.count()
+    return HttpResponse("Number of works {0}".format(number_of_works))
 
     
     
