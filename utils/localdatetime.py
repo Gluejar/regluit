a87f849a89c1e6d1adeba2ec5b873b5c845f291a
Raymond Yee <raymond.yee@gmail.com>
2012-03-08 11:04:25 -0800

    Implemented regluit.utils.localdatetime.now is by default datetime.datetime.now() (in Django 1.3) and django.utils.timezone.now() in Django 1.4+ One overrides regluit.utils.localdatetime._now to do "time travel" -- useful for testing scenarios requiring time changes (such as Campaigns)

---
+++ utils/localdatetime.py
@@ -0,0 +1,36 @@
+"""
+Utility to return datetime.datetime.utcnow() by default but allows for a custom utcnow() (e.g., for testing)
+
+>>> import regluit
+>>> from regluit.utils.localdatetime import now
+>>> now()
+datetime.datetime(2012, 3, 8, 14, 0, 35, 409270)
+>>> now()
+datetime.datetime(2012, 3, 8, 14, 0, 36, 985271)
+>>> n = now()
+>>> n
+datetime.datetime(2012, 3, 8, 14, 1, 54, 650679)
+>>> regluit.utils.localdatetime._now = lambda: n
+>>> now()
+datetime.datetime(2012, 3, 8, 14, 1, 54, 650679)
+>>> now()
+datetime.datetime(2012, 3, 8, 14, 1, 54, 650679)
+>>> now()
+
+
+"""
+
+import datetime
+import django
+
+# for Django 1.3.x, return a timestamp naive now()
+# for Django 1.4 should switch to django.utils.timezone.now()
+# see https://code.djangoproject.com/browser/django/trunk/django/utils/timezone.py?rev=17642#L232
+
+try:
+    _now = django.utils.timezone.now
+except AttributeError, e:
+    _now = datetime.datetime.now
+    
+now = lambda: _now()
+
