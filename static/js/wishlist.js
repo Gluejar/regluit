477cf9eed0ddeef4e517c8427381cd37cb74444b
eric <eric@hellman.net>
2012-03-09 13:59:24 -0500

    no divs allowed in content of span [#26203269]
    check for css/less mangling, pixel positioning of GR empblem

--- static/js/wishlist.js
+++ static/js/wishlist.js
@@ -6,7 +6,7 @@ $j().ready(function() {
 	
     contentblock.on("click", "div.add-wishlist", function () {
         var span = $j(this).find("span");
-        var gb_id = span.attr('id')
+        var gb_id = span.attr('id').substring(1)
         if (!gb_id) return;
             
         // give immediate feedback that action is in progress
@@ -21,7 +21,7 @@ $j().ready(function() {
     contentblock.on("click", "div.remove-wishlist", function() {
         var span = $j(this).find("span");
         var book = $j(this).closest('.thewholebook');
-        var work_id = span.attr('id')
+        var work_id = span.attr('id').substring(1)
         span.html('Removing...');
         jQuery.post('/wishlist/', {'remove_work_id': work_id}, function(data) {
             book.fadeOut();
