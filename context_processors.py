92381d8ab5cb4403929af3c29627e249f36c5a2c
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-03-29 15:34:03 -0400

    added an unseen_count variable available to all templates for notification signaling

--- context_processors.py
+++ context_processors.py
@@ -1,3 +1,11 @@
 def is_preview(request):
 	from django.conf import settings
 	return {'is_preview': settings.IS_PREVIEW, 'jquery_home': settings.JQUERY_HOME, 'jquery_ui_home': settings.JQUERY_UI_HOME}
+	
+def count_unseen(request):
+	from notification import NoticeManager
+	if request.user.is_anonymous():
+		count = 0
+	else:
+		count = NoticeManager.unseen_count_for(request.user)
+	return {'unseen_count': count }
