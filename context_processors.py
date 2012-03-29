00e9ee2e3c2a41b43caeda82cedde3195a858354
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-03-29 16:25:35 -0400

    now, with fewer syntax errors

--- context_processors.py
+++ context_processors.py
@@ -3,9 +3,9 @@ def is_preview(request):
 	return {'is_preview': settings.IS_PREVIEW, 'jquery_home': settings.JQUERY_HOME, 'jquery_ui_home': settings.JQUERY_UI_HOME}
 	
 def count_unseen(request):
-	from notification import NoticeManager
+	from notification.models import Notice, NoticeManager
 	if request.user.is_anonymous():
 		count = 0
 	else:
-		count = NoticeManager.unseen_count_for(request.user)
-	return {'unseen_count': count }
+		count = Notice.objects.unseen_count_for(request.user)
+	return {'unseen_count': count}
