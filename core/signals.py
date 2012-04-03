87bcf412d93d7665f7b6491ca5dac4c2ef5b7892
eshellman <eric@hellman.net>
2012-04-03 12:05:50 -0300

    really remove campaign activate signal

--- core/signals.py
+++ core/signals.py
@@ -87,23 +87,3 @@ def notify_comment(comment, request, **kwargs):
     tasks.emit_notifications.delay()
 
 comment_was_posted.connect(notify_comment)
-
-# need to have a signal specific to campaign activation
-# post_save isn't good enough as it will trigger whenever a campaign state changes
-signal_campaign_activated = django.dispatch.Signal(providing_args=["just_activated"])
-from regluit.core.models import Campaign
-def notify_active_campaign(sender, **kwargs):
-	just_activated = kwargs.get('just_activated', False)
-	print just_activated
-	if just_activated:
-	    # what sort of error handling do I need to do here? any?
-	    # logging?
-	    campaign = sender
-	    work = campaign.work
-	    # assumes only one active claim per campaign. safe?
-	    rightsholder = work.claim.filter(status="active")[0].rights_holder.rights_holder_name
-	    ungluers = work.wished_by()
-	    notification.queue(ungluers, "active_campaign", {'campaign':campaign, 'work':work, 'rightsholder':rightsholder}, True)
-	    import regluit.core.tasks as tasks 
-	    tasks.emit_notifications.delay()
-signal_campaign_activated.connect(notify_active_campaign)
