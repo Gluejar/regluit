b9c272ae74d1c193e000d16ed4ee33179d07e475
Raymond Yee <raymond.yee@gmail.com>
2012-04-18 15:12:15 -0700

    Removing stuff I had put into ry branch, specifically the experimental branch

--- experimental/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-
-if not settings.IS_PREVIEW:
-    urlpatterns = patterns(
-        "regluit.experimental.views",
-        url(r"^experimental/hello", "testExperimental"),       
-)
