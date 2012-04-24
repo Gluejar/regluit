a40e694faf7df76d151a34692314304060b13e8b
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-04-24 12:54:05 -0400

    some phrasing help, plus made the offer details field not required

--- core/models.py
+++ core/models.py
@@ -103,7 +103,7 @@ class Campaign(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=500, null=True, blank=False)
     description = models.TextField(null=True, blank=False)
-    details = models.TextField(null=True, blank=False)
+    details = models.TextField(null=True, blank=True)
     target = models.DecimalField(max_digits=14, decimal_places=2, null=True, blank=False)
     license = models.CharField(max_length=255, choices = LICENSE_CHOICES, default='CC BY-NC-ND')
     left = models.DecimalField(max_digits=14, decimal_places=2, null=True, blank=False)
