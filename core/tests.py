b21787a590e6438bb5b1133164ede574bb04ba86
eric <eric@hellman.net>
2012-05-07 01:14:47 -0400

    Revert "Revert "Revert "[#28945859] removed description from edition. Be sure to do data migration before applying this schema migration"""
    This reverts commit c8be251ffa84dc49f233ab4f6cbd7eb67b8973dd.

--- core/tests.py
+++ core/tests.py
@@ -102,7 +102,7 @@ class BookLoaderTests(TestCase):
         self.assertTrue(edition.work.publication_date)
         edition.publication_date = None
         self.assertTrue(edition.work.publication_date)
-        self.assertTrue(len(edition.work.description) > 20)
+        self.assertTrue(len(edition.work.longest_description()) > 20)
         self.assertTrue(edition.work.identifiers.filter(type='oclc')[0])
         
 
@@ -281,10 +281,9 @@ class BookLoaderTests(TestCase):
         subjects = [s.name for s in work.subjects.all()]
         self.assertTrue(len(subjects) > 10)
         self.assertTrue('Science fiction' in subjects)
-        self.assertTrue('/works/OL27258W' in work.identifiers.filter(type='olwk').values_list('value',flat=True) )
-        self.assertTrue('14770' in work.identifiers.filter(type='gdrd').values_list('value',flat=True))
-        self.assertTrue('609' in work.identifiers.filter(type='ltwk').values_list('value',flat=True))
-
+        self.assertEqual(work.openlibrary_id, '/works/OL27258W')
+        self.assertEqual(work.goodreads_id, '14770')
+        self.assertEqual(work.librarything_id, '609')
     def test_load_gutenberg_edition(self):
         """Let's try this out for Moby Dick"""
         
