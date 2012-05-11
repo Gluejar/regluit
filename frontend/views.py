ba36838dfa407aa1bfcb12d935564ea2d8324da6
Raymond Yee <raymond.yee@gmail.com>
2012-05-11 14:03:28 -0700

    must import notification

--- frontend/views.py
+++ frontend/views.py
@@ -54,6 +54,7 @@ from regluit.payment.paypal import Preapproval
 from regluit.core import goodreads
 from tastypie.models import ApiKey
 from regluit.payment.models import Transaction
+import notification
 
 
 logger = logging.getLogger(__name__)
