20a99412abdec003edcd11cbf624ad97cf9b5370
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-05-17 09:24:37 -0400

    streamlining percent_of_goal

--- core/models.py
+++ core/models.py
@@ -418,7 +418,7 @@ class Work(models.Model):
         status = 0
         if self.last_campaign() is not None:
             if(self.last_campaign_status() == 'SUCCESSFUL'):
-                status = 6;
+                status = 6
             elif(self.last_campaign_status() == 'ACTIVE'):
                 target = float(self.campaigns.order_by('-created')[0].target)
                 if target <= 0:
@@ -429,27 +429,21 @@ class Work(models.Model):
                     if percent >= 6:
                         status = 6
                     else:
-                        status = percent;
-        return status;
+                        status = percent
+        return status
 
-    def percent_unglued_number(self):
-        status = 0
-        if self.last_campaign() is not None:
-            if(self.last_campaign_status() == 'SUCCESSFUL'):
-                status = 100;
-            elif(self.last_campaign_status() == 'ACTIVE'):
-                target = float(self.campaigns.order_by('-created')[0].target)
-                if target <= 0:
-                    status = 100
-                else:
-                    total = float(self.campaigns.order_by('-created')[0].current_total)
-                    percent = int(total*100/target)
-                    if percent >= 100:
-                        status = 100
-                    else:
-                        status = percent;
-        return status;
-    
+    def percent_of_goal(self):
+        percent = 0
+        campaign = self.last_campaign()
+        if campaign is not None:
+            if(campaign.status == 'SUCCESSFUL'):
+                percent = 100
+            elif(campaign.status == 'ACTIVE'):
+                target = campaign.target
+                total = campaign.current_total
+                percent = int(total/target)
+        return percent
+
     def ebooks(self):
         return Ebook.objects.filter(edition__work=self).order_by('-created')
     
