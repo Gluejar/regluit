1ca930440caebcbb5e9f24147f5306db06c30c95
Raymond Yee <raymond.yee@gmail.com>
2012-05-17 13:54:09 -0700

    certain test views that should be on only if DEBUG = True

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-
 urlpatterns = patterns(
     "regluit.payment.views",
     url(r"^handleipn/(?P<module>\w+)$", "handleIPN", name="HandleIPN"),
@@ -13,7 +12,9 @@ urlpatterns += patterns(
      url(r"^amazonpaymentreturn", "amazonPaymentReturn", name="AmazonPaymentReturn"),
 )
 
-if not settings.IS_PREVIEW:
+# this should be on only if DEBUG is on
+
+if settings.DEBUG:
     urlpatterns += patterns(
         "regluit.payment.views",
         url(r"^testpledge", "testPledge"),
