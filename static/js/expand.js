120e5032228b72ae2a3f7cf668893ac71ee72898
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-05-21 12:09:45 -0400

    should fade everything in background, including launch_top

--- static/js/expand.js
+++ static/js/expand.js
@@ -6,12 +6,14 @@ $j(document).ready(function(){
     }
     $j('#expander').click(function(){
         $j('#js-topsection').css({"opacity": "0.15"});
+		$j('.launch_top').css({"opacity": "0.15"});
         $j('#main-container').css({"opacity": "0.15"});
         $j('#js-rightcol').css({"visibility":"hidden"});
         $j('#expandable').css({"position": "absolute", "z-index": "100", "left":"50%", "margin-left": "-115px"}).fadeTo("slow", 1);
     });
     $j('#collapser').click(function(){
         $j('#js-topsection').fadeTo("slow", 1);
+        $j('.launch_top').fadeTo("slow", 1);
         $j('#main-container').fadeTo("slow", 1);
         $j('#js-rightcol').css({"visibility":"visible"});
         $j('#expandable').css({"display": "none"});
