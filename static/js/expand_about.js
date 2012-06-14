2beb4e5dbb8362fd926d2c04279674b5141bfb5b
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-06-14 17:15:11 -0300

    commenting

--- static/js/expand_about.js
+++ static/js/expand_about.js
@@ -2,8 +2,29 @@ var $j = jQuery.noConflict();
 
 $j(document).ready(function(){
 	$j('.about_expander').click(function(){
+		// decide which about content to show
 		var whichbox = $j(this).attr('id');
-		$j('#lightbox').load('/static/html/'+whichbox+'.html');
+
+		// if we're on a supporter page, personalize our about box
+		// by writing the supporter's name in
+		if ($j(location).attr('pathname').slice(0,11) == '/supporter/') {
+		    var ungluer = $j(location).attr('pathname').slice(11, -1);
+		}
+		
+		if (ungluer != null) {
+		    $j('#lightbox').load('/static/html/'+whichbox+'.html');
+		    
+		    // span.ungluer doesn't exist until the ajax call so we
+		    // can't bind to the DOM on document ready; need to use
+		    // the ajaxComplete event
+		    $j('#lightbox').ajaxComplete(function() {
+		    	$j('#lightbox span.ungluer').replaceWith(ungluer);
+		    });
+		} else {
+		    $j('#lightbox').load('/static/html/'+whichbox+'.html');
+		}
+		
+		// fade-out (fade-in) rest of page elements on expand (collapse)
 		$j('#js-topsection').css({"opacity": "0.07"});
 		$j('.launch_top').css({"opacity": "0.07"});
 		$j('#main-container').css({"opacity": "0.07"});
