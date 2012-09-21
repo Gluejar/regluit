c4a2db66f7238f1ff240e0ca42f0014b6e7469c5
eric <eric@hellman.net>
2012-09-21 19:54:43 -0400

    Fix index out of bounds errors when no unglued epub is available [#36156381]

--- frontend/views.py
+++ frontend/views.py
@@ -2065,7 +2065,7 @@ def download(request, work_id):
 
     unglued_ebooks = work.ebooks().filter(edition__unglued=True)
     other_ebooks = work.ebooks().filter(edition__unglued=False)
-    unglued_epub_url = unglued_ebooks.filter(format='epub')[0].url
+    unglued_epub_url =  work.ebooks().filter(format='epub')[0].url if work.ebooks().filter(format='epub').count() else None
 
     context.update({
         'unglued_ebooks': unglued_ebooks,
