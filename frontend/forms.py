17e110696d320bbd000749f8fa16d0d0fc6afd19
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-10-05 12:08:06 -0400

    titles can be a lot longer than 30 characters

--- frontend/forms.py
+++ frontend/forms.py
@@ -368,7 +368,7 @@ class DonateForm(forms.Form):
     preapproval_amount = forms.DecimalField( widget=forms.HiddenInput() )
     username = forms.CharField(max_length=30, required=True, widget=forms.HiddenInput() )
     work_id = forms.IntegerField(required=False,  widget=forms.HiddenInput() )
-    title = forms.CharField(max_length=30, required=False, widget=forms.HiddenInput() )
+    title = forms.CharField(max_length=200, required=False, widget=forms.HiddenInput() )
     
     
 class GoodreadsShelfLoadingForm(forms.Form):
