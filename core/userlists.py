eb7d77dc5d0d027c03474aa76baefa739c2fd68f
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-02-06 15:05:16 -0500

    don't display staff in ungluers list

--- core/userlists.py
+++ core/userlists.py
@@ -7,7 +7,7 @@ from regluit.core.models import Work
 def other_users(user, how_many):
     # do something more sophisticated later?
     # limit to candidates with nonempty wishlists
-    candidates = User.objects.filter(wishlist__wishes__gte=1).distinct()
+    candidates = User.objects.filter(wishlist__wishes__gte=1, is_staff=False).distinct()
     count = candidates.count()
     if count <= how_many :
         user_list = candidates[0: count]
@@ -20,21 +20,21 @@ def supporting_users(work, how_many):
     # do something more sophisticated sometime later
     count = work.num_wishes
     if count <= how_many :
-        user_list = work.wished_by()[0: count]
+        user_list = work.wished_by().filter(is_staff=False)[0: count]
     else :
         slice = random.random() * (count - how_many)
-        user_list = work.wished_by()[slice: slice+how_many]
+        user_list = work.wished_by().filter(is_staff=False)[slice: slice+how_many]
     return user_list
 
 def work_list_users(work_list, how_many):
     """return up to how_many users with one of the works on work_list in their wishlist"""
     #users = User.objects.filter(wishlist__works__in=work_list).distinct().reverse()
     # for MySQL, avoiding a nested query is more efficient: https://docs.djangoproject.com/en/dev/ref/models/querysets/#in
-    users = User.objects.filter(wishlist__works__in=list(work_list[:100])).distinct().reverse()
+    users = User.objects.filter(wishlist__works__in=list(work_list[:100]), is_staff=False).distinct().reverse()
     return users.all()[0:how_many]
 
 def campaign_list_users(campaign_list, how_many):
-    users = User.objects.filter(wishlist__works__campaigns__in=list(campaign_list[:100])).distinct().reverse()
+    users = User.objects.filter(wishlist__works__campaigns__in=list(campaign_list[:100]), is_staff=False).distinct().reverse()
     count = users.count()
     if count <= how_many :
         user_list = users[0: count]
