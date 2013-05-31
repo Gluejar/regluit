c797de93e6d22408301bf1eceed9f9fd5d6e26fa
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-05-31 10:46:26 -0400

    download instruction buttons now work via ajax too

--- static/js/download_page.js
+++ static/js/download_page.js
@@ -1,21 +1,22 @@
 var $j = jQuery.noConflict();
 
-$j(document).on('prettifyDownload', function(){
-    $j('.buttons').show();
-    $j('.instructions div:not(#trythis_div)').hide();
-    $j('.instructions h4').hide();
-    
-    $j('.buttons div').on('click', function() {
-        $j(this).removeClass('modify');
-        $j(this).siblings().addClass('modify');
-        var buttonID = $j(this).children('a').attr('id');
-        var divSelector = '#' + buttonID + '_div';
-        var activeDiv = $j(divSelector);
-        activeDiv.show();
-        activeDiv.siblings().hide();
-    });
-});
+// must load CSS rather than show/hide jQuery objects; can't trigger an event
+// on an element not present on pageload, so binding it via on is useless
+if (document.createStyleSheet) {
+    // make it work in IE <= 8
+    document.createStyleSheet('/static/css/enhanced_download_ie.css');
+}
+else {
+    $j('<link rel="stylesheet" type="text/css" href="/static/css/enhanced_download.css" />').appendTo('head'); 
+}
 
-// needs to work both when people go straight to /download and when they get there via hijax link
-// ergo can't fire on document ready; needs custom trigger
-$j(document).trigger('prettifyDownload');
+// browser has a better sense of DOM changes than jQuery, so user can trigger click element
+$j(document).on('click', '.buttons div', function() {
+    $j(this).removeClass('modify');
+    $j(this).siblings().addClass('modify');
+    var buttonID = $j(this).children('a').attr('id');
+    var divSelector = '#' + buttonID + '_div';
+    var activeDiv = $j(divSelector);
+    activeDiv.show();
+    activeDiv.siblings().hide();
+});
