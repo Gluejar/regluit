3f63ef336f821a71abb86cb2043a73cca92d9921
eric <eric@hellman.net>
2013-09-15 21:43:01 -0400

    introduce pyepub

---
+++ pyepub/tests.py
@@ -0,0 +1,33 @@
+import unittest
+import urllib2 
+from tempfile import NamedTemporaryFile
+from StringIO import StringIO
+from . import EPUB
+
+
+class EpubTests(unittest.TestCase):
+
+    def setUp(self):
+        # get a small epub test file as a file-like object
+        self.epub2file = NamedTemporaryFile(delete=False)
+        test_file_content = urllib2.urlopen('http://www.hxa.name/articles/content/EpubGuide-hxa7241.epub')
+        self.epub2file.write(test_file_content.read())
+        self.epub2file.seek(0)
+        
+        
+    def test_instantiation(self):
+        epub=EPUB(self.epub2file)
+        self.assertNotEqual(epub.filename, None)
+        self.assertEqual(len(epub.opf),4)
+        self.assertEqual(len(epub.opf[0]),11) #metadata items
+        self.assertEqual(len(epub.opf[1]),11) #manifest items
+        self.assertEqual(len(epub.opf[2]),8) #spine items
+        self.assertEqual(len(epub.opf[3]),3) #guide items
+        
+    def test_addpart(self):
+        epub=EPUB(self.epub2file,mode='a')
+        self.assertNotEqual(epub.filename, None)
+        part = StringIO('<?xml version="1.0" encoding="utf-8" standalone="yes"?>')
+        epub.addpart(part, "testpart.xhtml", "application/xhtml+xml", 2)
+        self.assertEqual(len(epub.opf[2]),9) #spine items
+        
