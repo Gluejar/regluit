58b664bc2b432d227d90ad3db01c9836e373ba54
eric <eric@hellman.net>
2014-03-07 16:58:53 -0500

    fix bug when creating new edition

--- frontend/views.py
+++ frontend/views.py
@@ -510,11 +510,12 @@ def new_edition(request, work_id, edition_id, by=None):
         form = None
         edition.new_author_names=request.POST.getlist('new_author')
         edition.new_subjects=request.POST.getlist('new_subject')
-        for author in edition.authors.all():
-            if request.POST.has_key('delete_author_%s' % author.id):
-                edition.authors.remove(author)
-                form = EditionForm(instance=edition, data=request.POST, files=request.FILES)
-                break
+        if edition.id:
+            for author in edition.authors.all():
+                if request.POST.has_key('delete_author_%s' % author.id):
+                    edition.authors.remove(author)
+                    form = EditionForm(instance=edition, data=request.POST, files=request.FILES)
+                    break
         if request.POST.has_key('add_author_submit'):
             new_author_name = request.POST['add_author'].strip()
             try:
