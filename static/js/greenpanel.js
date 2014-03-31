14759ac546f62a4f6a5d5ddcad43fced5946e65d
eric <eric@hellman.net>
2014-03-30 21:28:32 -0400

    always show learn more to anonymous users
    logged in users get still get the drop-down

--- static/js/greenpanel.js
+++ static/js/greenpanel.js
@@ -1,7 +1,7 @@
 var $j = jQuery.noConflict();
 
 $j().ready(function(){
-	var contentblock = $j('#content-block, #user-block-hide');
+	var contentblock = $j('#content-block, .user-block-hide');
 	contentblock.on('mouseenter', '.panelview', function() {
 		$j(this).children('.panelfront').removeClass('side1').addClass('side2');
 		$j(this).children('.panelback').removeClass('side2').addClass('side1');
