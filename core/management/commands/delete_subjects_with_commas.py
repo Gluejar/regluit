7cb0f473de43276364c1af6d191a87a438443568
eric <eric@hellman.net>
2015-01-30 11:33:22 -0500

    remove many-commas subjects, prevent their addition

---
+++ core/management/commands/delete_subjects_with_commas.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from regluit.core.models import Subject
+
+
+
+class Command(BaseCommand):
+    '''Have observed that if the subject has more than two commas in it, it probably means something else'''
+    help = "delete subjects whose names have more than 2 commas"
+    
+    def handle(self, **options):
+       comma_subjects = Subject.objects.filter(name__contains=",")
+
+       for subject in comma_subjects:
+            num_commas = len(subject.name.split(','))-1
+            if num_commas >2:
+                print subject.name
+                subject.delete()
