a3a1a72b463a5785c2a831257dd3594f72f20458
eric <eric@hellman.net>
2015-09-21 21:04:11 -0400

    add management command to convert ungluit subjects to bisac

---
+++ core/management/commands/subjects_to_bisac.py
@@ -0,0 +1,17 @@
+import string
+from django.core.management.base import BaseCommand
+from regluit.bisac.models import BisacHeading
+from regluit.core.models import Subject
+
+class Command(BaseCommand):
+    help = "add and convert existing subjects to Bisac"
+    
+    def handle(self, **options):
+        matches=0
+        for bisac_heading in BisacHeading.objects.all():
+            for subject in Subject.objects.filter(name=bisac_heading.full_label):
+                subject.authority='bisacsh'
+                subject.name = bisac_heading.full_label
+                subject.save()
+                matches += 1
+        print "%s bisac headings converted" % matches
