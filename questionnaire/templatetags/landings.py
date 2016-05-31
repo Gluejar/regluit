4df440ae46d8a3e0826d47d3818210e6d29a734a
eric <eric@hellman.net>
2016-05-31 17:46:08 -0400

    fix survey completion

--- questionnaire/templatetags/landings.py
+++ questionnaire/templatetags/landings.py
@@ -4,9 +4,12 @@ register = django.template.Library()
 
 @register.simple_tag(takes_context=True)
 def render_with_landing(context, text):
-    block_context = Context({'landing_object' : context['runinfo'].landing.content_object})
+    if not context.has_key('landing_object') and context.has_key('runinfo'):
+        context['landing_object'] = context['runinfo'].landing.content_object
     if text:
         template = Template(text)
-        return template.render(block_context)
+        return template.render(context)
+    
     else:
         return ''
+        
