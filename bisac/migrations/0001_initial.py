d8eb4297d99658de47bf79195dab5bf3fa317f5b
eric <eric@hellman.net>
2016-07-21 15:38:09 -0400

    migrations

--- bisac/migrations/0001_initial.py
+++ bisac/migrations/0001_initial.py
@@ -1,46 +1,31 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import mptt.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'BisacHeading'
-        db.create_table('bisac_bisacheading', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('full_label', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-            ('label', self.gf('django.db.models.fields.CharField')(max_length=60)),
-            ('notation', self.gf('django.db.models.fields.CharField')(max_length=9)),
-            ('parent', self.gf('mptt.fields.TreeForeignKey')(blank=True, related_name='children', null=True, to=orm['bisac.BisacHeading'])),
-            (u'lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            (u'rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            (u'tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            (u'level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-        ))
-        db.send_create_signal('bisac', ['BisacHeading'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'BisacHeading'
-        db.delete_table('bisac_bisacheading')
-
-
-    models = {
-        'bisac.bisacheading': {
-            'Meta': {'object_name': 'BisacHeading'},
-            'full_label': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'label': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
-            u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'notation': ('django.db.models.fields.CharField', [], {'max_length': '9'}),
-            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['bisac.BisacHeading']"}),
-            u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        }
-    }
-
-    complete_apps = ['bisac']
+    operations = [
+        migrations.CreateModel(
+            name='BisacHeading',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('full_label', models.CharField(unique=True, max_length=100)),
+                ('label', models.CharField(max_length=60)),
+                ('notation', models.CharField(max_length=9)),
+                ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('level', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='bisac.BisacHeading', null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
