24c1cf8a037db462f99fd30aa02c209fea2849ea
eric <eric@hellman.net>
2016-07-30 02:36:01 -0400

    refactor the models

--- core/parameters.py
+++ core/parameters.py
@@ -1,3 +1,5 @@
 (REWARDS, BUY2UNGLUE, THANKS) = (1, 2, 3)
 (INDIVIDUAL, LIBRARY, BORROWED, RESERVE, THANKED) = (1, 2, 3, 4, 5)
 TESTING = 0
+OFFER_CHOICES = ((INDIVIDUAL,'Individual license'),(LIBRARY,'Library License'))
+ACQ_CHOICES = ((INDIVIDUAL,'Individual license'),(LIBRARY,'Library License'),(BORROWED,'Borrowed from Library'), (TESTING,'Just for Testing'), (RESERVE,'On Reserve'),(THANKED,'Already Thanked'),)
