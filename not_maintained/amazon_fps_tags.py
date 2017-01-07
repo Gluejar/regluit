a8790b741a10582ff613c44057bdb3246bb2cf44
eric <eric@hellman.net>
2017-01-07 11:16:42 -0500

    if we need any of these files, they should move somewhere

--- not_maintained/amazon_fps_tags.py
+++
@@ -1,8 +0,0 @@
-"""
-Template tags for amazon_fps
-"""
-from django import template
-from billing.templatetags.amazon_fps_tags import amazon_fps
-register = template.Library()
-
-register.tag(amazon_fps)
