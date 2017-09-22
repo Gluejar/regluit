c3fd4347a5a3d4464a653635b3b36a31b0fa8e0c
eric <eric@hellman.net>
2017-09-22 14:53:32 -0400

    use single quotes

--- libraryauth/urls.py
+++ libraryauth/urls.py
@@ -20,23 +20,23 @@ class ExtraContextTemplateView(TemplateView):
         return context
 
 urlpatterns = [
-    url(r"^libraryauth/(?P<library_id>\d+)/join/$", views.join_library, name="join_library"),
-    url(r"^libraryauth/(?P<library_id>\d+)/deny/$", TemplateView.as_view(template_name='libraryauth/denied.html'),  name="bad_library"),
-    url(r"^libraryauth/(?P<library_id>\d+)/users/$", views.library, {'template':'libraryauth/users.html'}, name="library_users"),
-    url(r"^libraryauth/(?P<library_id>\d+)/admin/$", login_required(views.UpdateLibraryView.as_view()),  name="library_admin"),
-    url(r"^libraryauth/(?P<library_id>\d+)/login/$", views.login_as_library,  name="library_login"),
-    url(r"^libraryauth/create/$", login_required(views.CreateLibraryView.as_view()),  name="library_create"),
-    url(r"^libraryauth/list/$", ExtraContextTemplateView.as_view(
+    url(r'^libraryauth/(?P<library_id>\d+)/join/$', views.join_library, name='join_library'),
+    url(r'^libraryauth/(?P<library_id>\d+)/deny/$', TemplateView.as_view(template_name='libraryauth/denied.html'),  name='bad_library'),
+    url(r'^libraryauth/(?P<library_id>\d+)/users/$', views.library, {'template':'libraryauth/users.html'}, name='library_users'),
+    url(r'^libraryauth/(?P<library_id>\d+)/admin/$', login_required(views.UpdateLibraryView.as_view()),  name='library_admin'),
+    url(r'^libraryauth/(?P<library_id>\d+)/login/$', views.login_as_library,  name='library_login'),
+    url(r'^libraryauth/create/$', login_required(views.CreateLibraryView.as_view()),  name='library_create'),
+    url(r'^libraryauth/list/$', ExtraContextTemplateView.as_view(
                 template_name='libraryauth/list.html',
                 extra_context={'libraries_to_show':'approved'}
-            ),  name="library_list"),
-    url(r"^libraryauth/unapproved/$", ExtraContextTemplateView.as_view(
+            ),  name='library_list'),
+    url(r'^libraryauth/unapproved/$', ExtraContextTemplateView.as_view(
                 template_name='libraryauth/list.html', 
                 extra_context={'libraries_to_show':'new'}
-            ), name="new_libraries"),
+            ), name='new_libraries'),
     url(r'^accounts/register/$', views.CustomRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/superlogin/$', views.superlogin, name='superlogin'),
-    url(r"^accounts/superlogin/welcome/$", ExtraContextTemplateView.as_view(
+    url(r'^accounts/superlogin/welcome/$', ExtraContextTemplateView.as_view(
             template_name='registration/welcome.html',
             extra_context={'suppress_search_box': True,} 
         ) ), 
@@ -50,8 +50,8 @@ urlpatterns = [
           {'template_name': 'registration/activation_complete.html'}),
     url(r'^accounts/login-error/$', superlogin,
           {'template_name': 'registration/from_error.html'}),
-    url(r'^accounts/edit/$', views.edit_user, name="edit_user"),
-    url(r"^accounts/login/welcome/$", ExtraContextTemplateView.as_view(
+    url(r'^accounts/edit/$', views.edit_user, name='edit_user'),
+    url(r'^accounts/login/welcome/$', ExtraContextTemplateView.as_view(
             template_name='registration/welcome.html',
             extra_context={'suppress_search_box': True,} 
         ) ), 
