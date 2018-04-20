0a9d3c4cd593e003ca4a0e697f1bed011ee0f4ce
eric <eric@hellman.net>
2018-04-19 21:23:40 -0400

    delint

--- booxtream/__init__.py
+++ booxtream/__init__.py
@@ -12,17 +12,17 @@ from . exceptions import BooXtreamError
 class BooXtream(object):
     """ ``apikey``
 
-          The API key for your BooXtream account, obtained from BooXtream. Defaults to using 
+          The API key for your BooXtream account, obtained from BooXtream. Defaults to using
           settings.BOOXTREAM_API_KEY
 
         ``apiuser``
 
-          The username key for your BooXtream account, obtained from BooXtream. Defaults to using 
+          The username key for your BooXtream account, obtained from BooXtream. Defaults to using
           settings.BOOXTREAM_API_USER
-          
-          
+
+
         ``timeout``
-        
+
         passed to requests
     """
     def __init__(self,
@@ -35,10 +35,10 @@ class BooXtream(object):
             apiuser = settings.BOOXTREAM_API_USER
         self.endpoint = 'https://service.booxtream.com/'
         self.postrequest = partial(requests.post, timeout=timeout, auth=(apiuser,apikey))
-        
+
 
     def platform(self,  epubfile=None, epub=True, kf8mobi=False,  **kwargs):
-        """ Make an API request to BooXtream 
+        """ Make an API request to BooXtream
         ``self.apikey``, ``epubfile`` and the supplied ``kwargs``.
         Attempts to deserialize the XML response and return the download link.
 
@@ -54,41 +54,41 @@ class BooXtream(object):
             if hasattr(epubfile,'name') and str(epubfile.name).endswith('.epub'):
                 files= {'epubfile': (str(epubfile.name),epubfile)}
             else:
-                # give it a random file name so that kindlegen doesn't choke 
-                # needed for in-memory (StringIO) epubs 
+                # give it a random file name so that kindlegen doesn't choke
+                # needed for in-memory (StringIO) epubs
                 files= {'epubfile': ('%012x.epub' % random.randrange(16**12),epubfile)}
         else:
-             files={}      
+             files={}
         if settings.LOCAL_TEST:
             # fake it, so you can test other functions without hitting booxtream
             boox = Boox.objects.create(
-                    download_link_epub='https://github.com/eshellman/42_ebook/blob/master/download/42.epub?raw=true&extra=download.booxtream.com/', 
-                    download_link_mobi='https://github.com/eshellman/42_ebook/blob/master/download/42.mobi?raw=true', 
-                    referenceid= kwargs.get('referenceid'),
-                    downloads_remaining= kwargs.get('downloadlimit'),
-                    expirydays=kwargs.get('expirydays'),
-                )
+                download_link_epub='https://github.com/eshellman/42_ebook/blob/master/download/42.epub?raw=true&extra=download.booxtream.com/',
+                download_link_mobi='https://github.com/eshellman/42_ebook/blob/master/download/42.mobi?raw=true',
+                referenceid= kwargs.get('referenceid'),
+                downloads_remaining= kwargs.get('downloadlimit'),
+                expirydays=kwargs.get('expirydays'),
+            )
             return boox
 
         resp = self.postrequest(url, data=kwargs, files=files)
         doc = ElementTree.fromstring(resp.content)
 
         # it turns out an Error can have an Error in it
-        errors = doc.findall('.//Response/Error') 
+        errors = doc.findall('.//Response/Error')
         if len(errors) > 0:
             raise BooXtreamError(errors)
         download_link_epub = doc.find('.//DownloadLink[@type="epub"]')
         if download_link_epub is not None:
-            download_link_epub = download_link_epub.text  
+            download_link_epub = download_link_epub.text
         download_link_mobi = doc.find('.//DownloadLink[@type="mobi"]')
         if download_link_mobi is not None:
-            download_link_mobi = download_link_mobi.text 
+            download_link_mobi = download_link_mobi.text
         boox = Boox.objects.create(
-                download_link_epub=download_link_epub, 
-                download_link_mobi=download_link_mobi, 
-                referenceid= kwargs.get('referenceid'),
-                downloads_remaining= kwargs.get('downloadlimit'),
-                expirydays=kwargs.get('expirydays'),
-            )
+            download_link_epub=download_link_epub,
+            download_link_mobi=download_link_mobi,
+            referenceid= kwargs.get('referenceid'),
+            downloads_remaining= kwargs.get('downloadlimit'),
+            expirydays=kwargs.get('expirydays'),
+        )
         return boox
 
