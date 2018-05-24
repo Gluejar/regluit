22865cc7b3862f188e734800d0a373f52b8237f0
eric <eric@hellman.net>
2018-05-24 11:03:33 -0400

    try following directions

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='Regluit',
+    name='regluit',
     version='3.0.0',
     description='Web application for Unglue.it',
     author='Free Ebook Foundation',
