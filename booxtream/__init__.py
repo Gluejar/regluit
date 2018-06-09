6408c878c4813515293520d7c6af1896b30365d4
eric <eric@hellman.net>
2018-06-09 15:00:45 -0400

    pull test fixes from Gluejar/master

--- booxtream/__init__.py
+++ booxtream/__init__.py
@@ -1,9 +1,10 @@
-import requests
 import random
-from django.conf import settings
-from urllib import quote
 from functools import partial
+from urllib import quote
 from xml.etree import ElementTree
+
+import requests
+from django.conf import settings
 
 from django.apps import apps
 
@@ -34,10 +35,10 @@ class BooXtream(object):
         if not apiuser:
             apiuser = settings.BOOXTREAM_API_USER
         self.endpoint = 'https://service.booxtream.com/'
-        self.postrequest = partial(requests.post, timeout=timeout, auth=(apiuser,apikey))
+        self.postrequest = partial(requests.post, timeout=timeout, auth=(apiuser, apikey))
 
 
-    def platform(self,  epubfile=None, epub=True, kf8mobi=False,  **kwargs):
+    def platform(self, epubfile=None, epub=True, kf8mobi=False, **kwargs):
         """ Make an API request to BooXtream
         ``self.apikey``, ``epubfile`` and the supplied ``kwargs``.
         Attempts to deserialize the XML response and return the download link.
@@ -48,25 +49,25 @@ class BooXtream(object):
         Boox = apps.get_model('booxtream', 'Boox')
 
         url = self.endpoint + 'booxtream.xml'
-        kwargs['epub'] =  '1' if epub else '0'
+        kwargs['epub'] = '1' if epub else '0'
         kwargs['kf8mobi'] = '1' if kf8mobi else '0'
         if epubfile:
-            if hasattr(epubfile,'name') and str(epubfile.name).endswith('.epub'):
-                files= {'epubfile': (str(epubfile.name),epubfile)}
+            if hasattr(epubfile, 'name') and str(epubfile.name).endswith('.epub'):
+                files = {'epubfile': (str(epubfile.name), epubfile)}
             else:
                 # give it a random file name so that kindlegen doesn't choke
                 # needed for in-memory (StringIO) epubs
-                files= {'epubfile': ('%012x.epub' % random.randrange(16**12),epubfile)}
+                files= {'epubfile': ('%012x.epub' % random.randrange(16**12), epubfile)}
         else:
-             files={}
+             files = {}
         if settings.LOCAL_TEST:
             # fake it, so you can test other functions without hitting booxtream
             boox = Boox.objects.create(
                 download_link_epub='https://github.com/eshellman/42_ebook/blob/master/download/42.epub?raw=true&extra=download.booxtream.com/',
                 download_link_mobi='https://github.com/eshellman/42_ebook/blob/master/download/42.mobi?raw=true',
-                referenceid= kwargs.get('referenceid'),
-                downloads_remaining= kwargs.get('downloadlimit'),
-                expirydays=kwargs.get('expirydays'),
+                referenceid= kwargs.get('referenceid', '42'),
+                downloads_remaining=kwargs.get('downloadlimit', 10),
+                expirydays=kwargs.get('expirydays', 30),
             )
             return boox
 
@@ -86,9 +87,8 @@ class BooXtream(object):
         boox = Boox.objects.create(
             download_link_epub=download_link_epub,
             download_link_mobi=download_link_mobi,
-            referenceid= kwargs.get('referenceid'),
-            downloads_remaining= kwargs.get('downloadlimit'),
+            referenceid=kwargs.get('referenceid'),
+            downloads_remaining=kwargs.get('downloadlimit'),
             expirydays=kwargs.get('expirydays'),
         )
         return boox
-
