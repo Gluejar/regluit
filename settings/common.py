961c8eebb2528f9f2368f74bfb8bccd6964b1756
eric <eric@hellman.net>
2018-07-26 11:25:51 -0400

    remove debug context processor
    raises exception when used without a request

--- settings/common.py
+++ settings/common.py
@@ -106,7 +106,6 @@ TEMPLATES = [
         'OPTIONS':{
             'context_processors':[
                 'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
