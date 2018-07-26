bf004702b829fc9ff9f732498862998f4ff81d85
eric <eric@hellman.net>
2018-07-26 17:32:56 -0400

    use the all-fixed dj-notification

--- settings/common.py
+++ settings/common.py
@@ -106,6 +106,7 @@ TEMPLATES = [
         'OPTIONS':{
             'context_processors':[
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
