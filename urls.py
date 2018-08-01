20fb2c5855e11cdb953689a5229e7ddef1739af4
eric <eric@hellman.net>
2018-08-01 09:38:49 -0400

    delint

--- urls.py
+++ urls.py
@@ -9,9 +9,9 @@ from regluit.admin import site
 from regluit.core.sitemaps import WorkSitemap, PublisherSitemap
 
 sitemaps = {
-        'works': WorkSitemap,
-        'publishers': PublisherSitemap,
-    }
+    'works': WorkSitemap,
+    'publishers': PublisherSitemap,
+}
 
 urlpatterns = [
     url(r'^api/', include('regluit.api.urls')),
@@ -21,11 +21,15 @@ urlpatterns = [
     url(r'', include('regluit.marc.urls')),
     url(r'^bisac/', include('regluit.bisac.urls')),
     url(r'^selectable/', include('selectable.urls')),
-    url(r'^admin/', site.urls), 
+    url(r'^admin/', site.urls),
     url(r'^comments/', include('django_comments.urls')),
     url(r"^notification/", include('notification.urls')),
     url(r'^ckeditor/upload/', login_required(ckedit_views.upload), name='ckeditor_upload'),
-    url(r'^ckeditor/browse/', never_cache(login_required(ckedit_views.browse)), name='ckeditor_browse'),
+    url(
+        r'^ckeditor/browse/',
+        never_cache(login_required(ckedit_views.browse)),
+        name='ckeditor_browse'
+    ),
     # questionnaire urls
     url(r'^survey/', include('questionnaire.urls')),
     # sitemaps
