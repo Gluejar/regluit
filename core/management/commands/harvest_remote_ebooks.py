2e4ce9772e12e2162fde6330aafd30d95fc51cb4
eric <eric@hellman.net>
2020-08-24 17:58:07 -0400

    add harvesting of downloadable ebooks

---
+++ core/management/commands/harvest_remote_ebooks.py
@@ -0,0 +1,60 @@
+from random import shuffle
+from django.core.management.base import BaseCommand
+
+from regluit.core.loaders.harvest import archive_dl, RateLimiter, DONT_HARVEST
+from regluit.core.models import Ebook
+from regluit.core.parameters import GOOD_PROVIDERS
+DOWNLOADABLE = ['pdf', 'epub', 'mobi']
+
+DONT_CHECK = list(GOOD_PROVIDERS) + DONT_HARVEST
+
+class Command(BaseCommand):
+    help = "check/harvest ebooks from 'remote' ebooks"
+    args = "<limit>"
+
+    def add_arguments(self, parser):
+        parser.add_argument('limit', nargs='?', type=int, default=0, help="max to harvest")
+        parser.add_argument('--ebook', nargs='?', type=int, default=0, help="ebook to harvest")
+        parser.add_argument('--provider', nargs='?', type=str, default='', help="provider to harvest")
+        parser.add_argument('--format', nargs='?', type=str, default='all', help="format to harvest")
+
+    def handle(self, limit=0, **options):
+        limit = int(limit) if limit else 0
+        rl = RateLimiter()
+        format = options.get('format')
+        if format == 'all':
+            onlines = Ebook.objects.filter(format__in=DOWNLOADABLE)
+        else:
+            onlines = Ebook.objects.filter(format=format)
+        if options.get('ebook'):
+            onlines = Ebook.objects.filter(id=options.get('ebook'))
+        elif options.get('provider'):
+            onlines = onlines.filter(provider=options.get('provider'))
+        else:
+            onlines = onlines.exclude(provider__in=DONT_CHECK)
+            online_ids = [ebook.id for ebook in onlines]
+            self.stdout.write('%s ebooks need checking.' % len(onlines))
+            shuffle(online_ids)
+            onlines = (Ebook.objects.get(id=id) for id in online_ids)
+        archived = {}
+        failed = {}
+        done = 0
+        for online in onlines:
+            status = archive_dl(online, limiter=rl.delay, format=format)
+            if status ==  1:
+                done += 1
+                archived[online.provider] = archived.get(online.provider, 0) + 1
+                self.stdout.write(online.edition.title)
+            elif status == -1:
+                done += 1
+                failed[online.provider] = failed.get(online.provider, 0) + 1
+                online.format = 'online'
+                online.active = False
+                online.save()
+            if done >= limit or done >= 500:
+                break
+        self.stdout.write("archived")
+        for result in [archived, failed]:
+            for provider in result:
+                self.stdout.write('%s\t%s' % (provider, result[provider]))
+            self.stdout.write("failed")
