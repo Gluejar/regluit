6e755957140e280a3e8849699c8dc07c504f60cd
eric <eric@hellman.net>
2020-10-02 17:44:53 -0400

    see if this lets notification work

--- core/tasks.py
+++ core/tasks.py
@@ -108,7 +108,7 @@ def update_active_campaign_status():
 @task
 def emit_notifications():
     logger.info('notifications emitting' )
-    return send_all()
+    call_command('emit_notices')
     
 @task
 def report_new_ebooks(created=None):   #created= creation date
