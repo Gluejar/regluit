7f8682f9811bfe75fdfb07e13f290ea96f7a2be4
eric <eric@hellman.net>
2021-03-14 14:44:21 -0400

    add trace option to harvest commands
    needed to help debug OOM errors

--- core/management/commands/harvest_online_ebooks.py
+++ core/management/commands/harvest_online_ebooks.py
@@ -13,11 +13,13 @@ class Command(BaseCommand):
         parser.add_argument('--ebook', nargs='?', type=int, default=0, help="ebook to harvest")
         parser.add_argument('--provider', nargs='?', default='', help="provider to harvest")
         parser.add_argument('--format', nargs='?', default='online', help="format to harvest")
+        parser.add_argument('--trace', action='store_true', help="trace")
 
-    def handle(self, limit=0, **options):
+    def handle(self, limit=0, trace=False, **options):
         limit = int(limit) if limit else 0
         rl = RateLimiter()
         format = options.get('format')
+        trace = options.get('trace')
         if options.get('ebook'):
             onlines = Ebook.objects.filter(id=options.get('ebook'))
         elif options.get('provider'):
@@ -27,13 +29,18 @@ class Command(BaseCommand):
             shuffle(online_ids)
             onlines = (Ebook.objects.get(id=id) for id in online_ids)
         done = 0
+        providers = {}
         for online in onlines:
+            if trace:
+                self.stdout.write(str(online.id))
             new_ebf, new = dl_online(online, limiter=rl.delay, format=format)
             if new_ebf and new:
                 done += new
+                providers[online.provider] = providers.get(online.provider, 0) + 1
                 self.stdout.write(new_ebf.edition.work.title)
                 if done >= limit or done >= 500:
                     break
         self.stdout.write('harvested {} ebooks'.format(done))
+        self.stdout.write(providers)
         if done >= 500:
             self.stdout.write('500 is the maximum; repeat to do more')
