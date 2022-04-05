5e2560835a742e8719c939ea0e908226acb1591c
eric <eric@hellman.net>
2022-04-05 12:29:51 -0400

    fix mdpi

--- core/loaders/harvest.py
+++ core/loaders/harvest.py
@@ -733,13 +733,9 @@ def harvest_mdpi(ebook):
         return None, 0
     elif 'img.mdpi.org' in ebook.url:
         return harvest_generic(ebook)
-    elif ebook.url.find(u'mdpi.com/books/pdfview/book/') >= 0:
-        doc = get_soup(ebook.url)
-        if doc:
-            obj = doc.find('object', type='application/pdf')
-            if obj and 'data' in obj:
-                return make_dl_ebook(obj['data'], ebook)
-            return None, 0   
+    elif ebook.url.find('mdpi.com/books/pdfview/book/') >= 0:
+        url = ebook.url.replace('pdfview', 'pdfdownload')
+        return make_dl_ebook(url, ebook)
     return harvest_one_generic(ebook, selector)
 
 
