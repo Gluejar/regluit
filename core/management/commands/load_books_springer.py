f23c244d540d272426ad60b33accf9efbf630a00
eric <eric@hellman.net>
2022-05-31 10:43:09 -0400

    update code for Springer

--- core/management/commands/load_books_springer.py
+++ core/management/commands/load_books_springer.py
@@ -1,15 +1,20 @@
 from django.core.management.base import BaseCommand
 
-from regluit.core.loaders.springer import load_springer
+from regluit.core.loaders.springer import load_springer, SpringerScraper
+from regluit.core.bookloader import add_from_bookdatas
 
 class Command(BaseCommand):
     help = "load books from springer open"
 
     def add_arguments(self, parser):
         parser.add_argument('startpage', nargs='?', type=int, default=1, help="page to start on")    
-        parser.add_argument('endpage', nargs='?', type=int, default=1, help="page to end on")    
-   
+        parser.add_argument('endpage', nargs='?', type=int, default=1, help="page to end on")
+        parser.add_argument('--url', nargs='?', default='', help="url to scrape")
+      
 
     def handle(self, startpage, endpage=0, **options):        
-        books = load_springer(int(startpage), int(endpage))       
+        if options.get('url'):
+            books = add_from_bookdatas([SpringerScraper(options.get('url'))])
+        else:
+            books = load_springer(int(startpage), int(endpage))       
         self.stdout.write("loaded {} books".format(len(books)))
